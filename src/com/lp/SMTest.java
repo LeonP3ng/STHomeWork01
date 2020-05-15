c72b0a314ad3b4c20f7506191fc1195f8a708534
LeonP3ng <993179874@qq.com>
2020-05-15 10:09:57 +0800

    代码审查修正版

--- src/com/lp/SMTest.java
+++ src/com/lp/SMTest.java
@@ -2,8 +2,10 @@ package com.lp;
 
 public class SMTest {
     public static void main(String[] args) {
-        StudentManager studentManager =new StudentManager();
-        studentManager.App();
+        //参考《Google Java编程风格指南》6.3 静态成员：使用类进行调用
+        //应使用类名调用静态的类成员，而不是具体某个对象或表达式
+        //StudentManager studentManager =new StudentManager();
+        StudentManager.App();
         
     }
 }
--- src/com/lp/StudentManager.java
+++ src/com/lp/StudentManager.java
@@ -3,18 +3,32 @@ package com.lp;
 import java.util.*;
 
 public class StudentManager {
-    ArrayList<Student> studentList = new ArrayList<Student>();
-    void App(){
+    static ArrayList<Student> studentList = new ArrayList<Student>();
+
+    //7 11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**
+     * 功能：主函数，接收用户输入的数字，执行相应功能
+     * 输入：无
+     * 输出：无
+     * */
+    static void App(){
         while (true){
             System.out.println("");
             System.out.println("请选择操作：");
             System.out.println("***********************************");
-            System.out.println("*                           1  插入                                  *");
-            System.out.println("*                           2  查找                                  *");
-            System.out.println("*                           3  删除                                  *");
-            System.out.println("*                           4  修改                                  *");
-            System.out.println("*                           5  输出                                  *");
-            System.out.println("*                           6  退出                                  *");
+            // 19 代码行长度是否在要求之内？ 参考《Google Java编程风格指南》4.4 同一行里的字符长度不超过100
+            //System.out.println("*                           1  插入                                  *");
+            //System.out.println("*                           2  查找                                  *");
+            //System.out.println("*                           3  删除                                  *");
+            //System.out.println("*                           4  修改                                  *");
+            //System.out.println("*                           5  输出                                  *");
+            //System.out.println("*                           6  退出                                  *");
+            System.out.println("*                           1  插入                             *");
+            System.out.println("*                           2  查找                             *");
+            System.out.println("*                           3  删除                             *");
+            System.out.println("*                           4  修改                             *");
+            System.out.println("*                           5  输出                             *");
+            System.out.println("*                           6  退出                             *");
             System.out.println("***********************************");
 
             Scanner sc = new Scanner(System.in);
@@ -46,15 +60,26 @@ public class StudentManager {
                 case "2":{
                     System.out.println("请输入学生姓名：");
                     int count = getStudentsByName(sc.nextLine());
-                    if(count !=0 ) System.out.println("共找到"+ count+"条记录");
-                    else System.out.println("查无此人!");
+                    //23
+                    //if(count !=0 ) System.out.println("共找到"+ count+"条记录");
+                    //else System.out.println("查无此人!");
+                    if(count !=0 ) {
+                        System.out.println("共找到"+ count+"条记录");
+                    }else {
+                        System.out.println("查无此人!");
+                    }
                     break;
                 }
                 case "3":{
                     System.out.println("请输入学生姓名：");
                     int count = deleteStudentsByName(sc.nextLine());
-                    if(count !=0 ) System.out.println("共删除"+ count+"条记录");
-                    else System.out.println("查无此人!");
+                    //18 23
+                    //if(count !=0 ) System.out.println("共删除"+ count+"条记录");
+                    if(count != 0 ) {
+                        System.out.println("共删除"+ count+"条记录");
+                    } else {
+                        System.out.println("查无此人!");
+                    }
                     break;
                 }
                 case "4":{
@@ -66,19 +91,25 @@ public class StudentManager {
                     student.setBirDate(sc.nextLine());
                     System.out.println("请输入学生修改后性别：");
                     String geneder = sc.nextLine();
-                    if(geneder.equals("男"))
+                    //23
+                    // if(geneder.equals("男"))
+                    if(geneder.equals("男")) {
                         student.setGeneder(true);
-                    else if(geneder.equals("女"))
+                    }else if(geneder.equals("女")) {
                         student.setGeneder(false);
-                    else {
+                    }else {
                         System.out.println("添加失败，请正确输入性别！");
                         break;
                     }
 
                     int count = updateStudentsByName(student);
-                    if(count !=0 ) System.out.println("修改"+ count+"条记录");
-                    else System.out.println("查无此人！");
-
+                    //23
+                    //if(count !=0 ) System.out.println("修改"+ count+"条记录");
+                    if(count !=0 ) {
+                        System.out.println("修改"+ count+"条记录");
+                    } else {
+                        System.out.println("查无此人！");
+                    }
                     break;
                 }
                 case "5":{
@@ -98,8 +129,13 @@ public class StudentManager {
         }
 
     }
-    //1 插入
-    boolean insertStudent(Student student){
+
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**  功能:输入'1'，插入学生信息
+    *   输入：Student类型变量,学生实体
+    *   返回：布尔类型，插入成功返回true,插入失败返回false
+    * */
+    static boolean insertStudent(Student student){
         if(studentList.size() == 20) {
             System.out.println("插入失败，系统人数已达到上限20人！");
             return false;
@@ -128,9 +164,12 @@ public class StudentManager {
         return true;
     }
 
-
-    //2 查找
-    int getStudentsByName(String name){
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**  功能：输入'2'，通过名字查找学生记录
+     *  输入：String类型，学生名字
+     *  返回：int类型，返回操作影响的记录条数
+     * */
+    static int getStudentsByName(String name){
 
         int count = 0;
         for(Student sd : studentList)
@@ -143,16 +182,26 @@ public class StudentManager {
 
     }
 
-    //3 删除
-    int deleteStudentsByName(String name){
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /** 功能：输入'3'，通过名字删除学生记录
+    *  输入：String类型，学生名字
+    *  返回：int类型，返回操作影响的记录条数
+    * */
+
+    static int deleteStudentsByName(String name){
 
         int count = 0;
         int size = studentList.size();
         for(int i=0;i<size;i++) {
-            if(i>=studentList.size()) break; //防止一次删除多个元素，导致越界
+            //23
+            //if(i>=studentList.size()) break; //防止一次删除多个元素，导致越界
+            if(i>=studentList.size()) {
+                break;
+            }
             Student student = studentList.get(i);
             if(student.getName().equals(name)) {
                 count++;
+                //60
                 studentList.remove(student);
             }
         }
@@ -160,8 +209,13 @@ public class StudentManager {
         return count;
     }
 
-    //4 修改
-    int updateStudentsByName(Student student){
+
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**  功能：输入'4'，通过名字修改学生记录
+     *  输入：Student类型，学生实体
+     *  返回：int类型，返回操作影响的记录条数
+     * */
+    static int updateStudentsByName(Student student){
         int count = 0;
         int size = studentList.size();
         for(int i=0;i<size;i++) {
@@ -172,21 +226,32 @@ public class StudentManager {
                 count++;
             }
         }
-
         return count;
     }
-    //5 输出
-    void outputStudents(){
-        for(Student student:studentList)
+
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**  功能：输入'5'，打印所有学生信息
+     *  输入：无
+     *  返回：无
+     * */
+    static void outputStudents(){
+        //23
+        //for(Student student:studentList)
+        //    System.out.println(student);
+        for(Student student:studentList){
             System.out.println(student);
+        }
+
         System.out.println("共计"+studentList.size()+"条记录");
     }
 
-
-    //6 退出
-    void exitStudents(){
+    //11 函数是否已经有文档注释？（功能、输入、返回及其他可选函数是否已经有文档注释？（功能、输入、返回及其他可选)
+    /**  功能：输入'6'，清空list，退出系统
+     *  输入：无
+     *  返回：无
+     * */
+    static void exitStudents(){
         studentList.clear();
-
     }
 
 
